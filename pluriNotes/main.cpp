4d02c4615f6f209191147efa5c7a4df409021f26
jberthomier <justine.berthomier@gmail.com>
2017-06-10 17:53:40 +0200

    Create main.cpp

--- pluriNotes/main.cpp
+++ pluriNotes/main.cpp
@@ -5,8 +5,7 @@ using namespace std;
 
 int main(int argc, char* argv[]){
     QApplication app(argc, argv);
-
-
-
+    PluriNotes* win = new PluriNotes();
+    win->show();
     return app.exec();
 }
